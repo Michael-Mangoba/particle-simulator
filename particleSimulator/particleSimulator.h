dc359ca6f506418f492d8cc19c696eb8d961c575
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-02-16 21:29:18 +0800

    Cleanup imports

--- particleSimulator/particleSimulator.h
+++ particleSimulator/particleSimulator.h
@@ -3,8 +3,5 @@
 #include "glad/glad.h"
 #include <vector>
 #include <iostream>
-//#include "imgui.h"
-//#include "imgui_impl_sdl2.h"
-//#include "imgui_impl_opengl3.h"
 #include <SDL.h>
 #include <thread>
